9db70840cd7bc30479acfd4ca68f19cd98b75770
ilalouch <33584171+ilalouch@users.noreply.github.com>
2019-12-24 17:40:35 +0200

    Create pdf_merger.py

---
+++ pdf_merger.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# program that combines three pdf files into a single one using PyPDF2
+
+import PyPDF2
+
+# open the pdf files for combining
+pdfFile1 = open('file1.pdf', 'rb')
+pdfFile2 = open('file2.pdf', 'rb')
+pdfFile3 = open('file3.pdf', 'rb')
+
+# read pdf contents
+reader1 = PyPDF2.PdfFileReader(pdfFile1)
+reader2 = PyPDF2.PdfFileReader(pdfFile2)
+reader3 = PyPDF2.PdfFileReader(pdfFile3)
+
+# to write combined file into a new file
+writer = PyPDF2.PdfFileWriter()
+
+# iterate over the pages in every pdf file and add them into the final document
+for pageNum in range(reader1.numPages):
+    page = reader1.getPage(pageNum)
+    writer.addPage(page)
+
+for pageNum in range(reader2.numPages):
+    page = reader2.getPage(pageNum)
+    writer.addPage(page)
+
+for pageNum in range(reader3.numPages):
+    page = reader3.getPage(pageNum)
+    writer.addPage(page)
+
+# output the merged pdf into the working directory
+combinedPdf = open('combinedpdf.pdf', 'wb')
+writer.write(output)
+output.close()
+
+# close files
+pdfFile1.close()
+pdfFile2.close()
+pdfFile3.close()
